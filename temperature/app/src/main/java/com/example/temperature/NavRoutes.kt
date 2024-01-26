8dfea3393ea4dfaa41af491bd691cf0946fbcb4a
vamshigunda3 <78726582+vamshigunda3@users.noreply.github.com>
2024-01-26 17:32:48 +0530

    Delete temperature directory

--- temperature/app/src/main/java/com/example/temperature/NavRoutes.kt
+++
@@ -1,7 +0,0 @@
-package com.example.temperature
-
-sealed class NavRoutes(val route: String) {
-    object Home : NavRoutes("home")
-    object Contacts : NavRoutes("contacts")
-    object Favorites : NavRoutes("favorites")
-}
