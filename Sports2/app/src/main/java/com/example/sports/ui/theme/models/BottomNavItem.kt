03ec5978a544354eea96de248318bbe745bf33dd
vamshigunda3 <78726582+vamshigunda3@users.noreply.github.com>
2024-01-26 17:32:01 +0530

    Delete Sports2 directory

--- Sports2/app/src/main/java/com/example/sports/ui/theme/models/BottomNavItem.kt
+++
@@ -1,9 +0,0 @@
-package com.example.sports.ui.theme.models
-
-import androidx.compose.ui.graphics.vector.ImageVector
-
-data class BottomNavItem(
-    val label: String,
-    val icon: ImageVector,
-    val route:String,
-)
